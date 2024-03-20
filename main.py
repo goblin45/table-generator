1b29db624828a5fc51b3d3b7ccedba0ec4f66c95
V3nomV1per <2003priyanshudas@gmail.com>
2024-03-20 23:12:34 +0530

    Added img to CSV to XLSX

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import cv2 as cv
 import numpy as np
-# import pytesseract
+import csv
+import pandas as pd
 import easyocr
 
-image = cv.imread('big-table.png', cv.IMREAD_GRAYSCALE)
-reader = easyocr.Reader(['en'])
+image = cv.imread("big-table.png", cv.IMREAD_GRAYSCALE)
+reader = easyocr.Reader(["en"])
 
 imgArr = np.array(image)
 
@@ -33,7 +34,7 @@ kernels = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     ],
     # bottom left corner
     [
@@ -46,7 +47,7 @@ kernels = [
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ],
     # bottom right corner
     [
@@ -59,7 +60,7 @@ kernels = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     ],
     # center corner
     # [
@@ -76,20 +77,19 @@ for i in range(len(kernels)):
     resImgArr = np.array(resImage)
     newImageArray.append(resImgArr)
 
-cv.waitKey(0)
 
 corners = set()
 
 for i in range(len(newImageArray)):
     currImgArr = newImageArray[i]
     rows, cols = currImgArr.shape
-    for row in range(rows): 
+    for row in range(rows):
         for col in range(cols):
             if currImgArr[row][col] == 0:
                 breakFlag = False
                 for col1, row1 in corners:
                     dist = np.sqrt((row1 - row) ** 2 + (col1 - col) ** 2)
-                    if dist < 15: 
+                    if dist < 15:
                         avgCol = (col + col1) // 2
                         avgRow = (row + row1) // 2
                         corners.add((avgCol, avgRow))
@@ -99,7 +99,7 @@ for i in range(len(newImageArray)):
                 if not breakFlag:
                     corners.add((col, row))
 
-corners = sorted(corners, key= lambda x: x[0])
+corners = sorted(corners, key=lambda x: x[0])
 
 cornersMat = []
 diff = 15
@@ -128,7 +128,7 @@ for row in range(len(cornersMat)):
         cornersMat[col][row] = temp
 
 res = []
-
+# subImageList = []
 for i in range(len(cornersMat) - 1):
     temp = []
     for j in range(len(cornersMat[0]) - 1):
@@ -138,19 +138,50 @@ for i in range(len(cornersMat) - 1):
         x_max = max(corner1[0], corner2[0], corner3[0], corner4[0])
         y_min = min(corner1[1], corner2[1], corner3[1], corner4[1])
         y_max = max(corner1[1], corner2[1], corner3[1], corner4[1])
-        subImgArr = imgArr[y_min - 2:y_max + 2, x_min - 2:x_max + 2]
+        subImgArr = imgArr[y_min - 2 : y_max + 2, x_min - 2 : x_max + 2]
+        # subImageList.append(subImgArr)
         subImage = np.uint8(subImgArr)
-        # cv.imshow('sub', subImage)
+        cv.imshow("SubIMG", subImage)
         cv.waitKey(0)
         extracted_text = reader.readtext(subImage)
-        if len(extracted_text) == 0:
-            temp.append("")
-        else:
-            string = ""
-            for item in extracted_text:
-                text = item[1]
-                string += text
-            temp.append(string)
+        # if len(extracted_text) == 0:
+        extra = 1
+        while len(extracted_text) == 0:
+            y_max += extra
+            y_min -= extra
+            x_max += extra
+            x_min -= extra
+            subImgArr = imgArr[y_min:y_max, x_min:x_max]
+            subImage = np.uint8(subImgArr)
+            extracted_text = reader.readtext(subImage)
+        # else:
+        string = ""
+        for item in extracted_text:
+            text = item[1]
+            string += text
+        temp.append(string)
     res.append(temp)
 
 print(res)
+
+resultCSV = "output_data.csv"
+
+
+def save_to_csv(res, resultCSV):
+    with open(resultCSV, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(res)
+
+
+save_to_csv(res, resultCSV)
+print(f"Data saved to {resultCSV}")
+
+new_dataFrame = pd.read_csv("output_data.csv")
+new_excel = pd.ExcelWriter("output_data.xlsx")
+new_dataFrame.to_excel(new_excel, index=False)
+new_excel._save()
+
+with open("output_data.csv") as file_obj:
+    reader_obj = csv.reader(file_obj)
+    for row in reader_obj:
+        print(row)
