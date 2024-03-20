0e450629642f6c4bbee7e98006fa095ee55ecd37
Rajarshi Dutta <gogolmf74@gmail.com>
2024-03-20 19:08:44 +0530

    Text extracted successfully
    Signed-off-by: Rajarshi Dutta <gogolmf74@gmail.com>

--- main.py
+++ main.py
@@ -1,34 +1,65 @@
 import cv2 as cv
 import numpy as np
+# import pytesseract
+import easyocr
 
-image = cv.imread('sample.png', cv.IMREAD_GRAYSCALE)
+image = cv.imread('big-table.png', cv.IMREAD_GRAYSCALE)
+reader = easyocr.Reader(['en'])
 
 imgArr = np.array(image)
 
 kernels = [
     # top left corner
     [
-        [1, 1, 1],
-        [1, 0, 0],
-        [1, 0, 0]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     ],
     # top right corner
     [
-        [1, 1, 1],
-        [0, 0, 1],
-        [0, 0, 1]
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
     ],
     # bottom left corner
     [
-        [1, 0, 0],
-        [1, 0, 0],
-        [1, 1, 1]
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ],
     # bottom right corner
     [
-        [0, 0, 1],
-        [0, 0, 1],
-        [1, 1, 1]
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
     ],
     # center corner
     # [
@@ -58,7 +89,7 @@ for i in range(len(newImageArray)):
                 breakFlag = False
                 for col1, row1 in corners:
                     dist = np.sqrt((row1 - row) ** 2 + (col1 - col) ** 2)
-                    if dist < 4: 
+                    if dist < 15: 
                         avgCol = (col + col1) // 2
                         avgRow = (row + row1) // 2
                         corners.add((avgCol, avgRow))
@@ -68,22 +99,58 @@ for i in range(len(newImageArray)):
                 if not breakFlag:
                     corners.add((col, row))
 
-# print(sorted(corners, key= lambda x: x[0]))
+corners = sorted(corners, key= lambda x: x[0])
 
-filteredImgArr = []
+cornersMat = []
+diff = 15
+prevCorner = corners[0]
+tempCorners = []
+for i in range(len(corners)):
+    currX, currY = corners[i]
+    if currX > prevCorner[0] + diff:
+        cornersMat.append(tempCorners)
+        tempCorners = [corners[i]]
+        prevCorner = corners[i]
+    else:
+        tempCorners.append(corners[i])
+if len(tempCorners) > 0:
+    cornersMat.append(tempCorners)
 
-for row in range(107):
+for i in range(len(cornersMat)):
+    cornersMat[i] = sorted(cornersMat[i], key=lambda x: x[1])
+
+diagonal = -1
+for row in range(len(cornersMat)):
+    diagonal += 1
+    for col in range(diagonal, len(cornersMat)):
+        temp = cornersMat[row][col]
+        cornersMat[row][col] = cornersMat[col][row]
+        cornersMat[col][row] = temp
+
+res = []
+
+for i in range(len(cornersMat) - 1):
     temp = []
-    for col in range(180):
-        if (col, row) in corners:
-            temp.append(0)
+    for j in range(len(cornersMat[0]) - 1):
+        corner1, corner2 = cornersMat[i][j], cornersMat[i][j + 1]
+        corner3, corner4 = cornersMat[i + 1][j], cornersMat[i + 1][j + 1]
+        x_min = min(corner1[0], corner2[0], corner3[0], corner4[0])
+        x_max = max(corner1[0], corner2[0], corner3[0], corner4[0])
+        y_min = min(corner1[1], corner2[1], corner3[1], corner4[1])
+        y_max = max(corner1[1], corner2[1], corner3[1], corner4[1])
+        subImgArr = imgArr[y_min - 2:y_max + 2, x_min - 2:x_max + 2]
+        subImage = np.uint8(subImgArr)
+        # cv.imshow('sub', subImage)
+        cv.waitKey(0)
+        extracted_text = reader.readtext(subImage)
+        if len(extracted_text) == 0:
+            temp.append("")
         else:
-            temp.append(255)
-    filteredImgArr.append(temp)
+            string = ""
+            for item in extracted_text:
+                text = item[1]
+                string += text
+            temp.append(string)
+    res.append(temp)
 
-filteredImage = np.uint8(filteredImgArr)
-height, width = filteredImage.shape
-resizedImage = cv.resize(filteredImage, (width * 2, height * 2))
-
-cv.imshow('result', resizedImage)
-cv.waitKey(0)
+print(res)
